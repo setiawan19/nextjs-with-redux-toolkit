81bd67a72542a27679f5ee2040b4f5b96950cc94
Mukri Setiawan Baguna <mukri.setiawan@flexidev.co>
2024-03-11 01:53:11 +0700

    update typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="flex flex-col min-h-screen gap-2">
       <div className="flex items-center justify-center w-full p-5">
-        <p className="font-bold text-2xl">NextJS Typescript With Redeux Toolkit</p>
+        <p className="font-bold text-2xl">NextJS Typescript With Redux Toolkit</p>
       </div>
       <div className="flex min-h-screen w-full items-start justify-between p-10 gap-2">
         <Sidebar />
